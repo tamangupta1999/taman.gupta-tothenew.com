51f2958003c1afff94cda7b56277fb73788bfd43
tamangupta1999 <taman.gupta@tothenew.com>
2020-03-11 10:22:11 +0530

    JS ES6 Session 2

--- main.js
+++ main.js
@@ -13,7 +13,29 @@ console.log(filteredValue);
 
 
 // Q.2 Find the possible combinations of a string and store them in a MAP? 
+function getPermutations(string) {
 
+    let results = [];
+
+    if(string.length === 1) {
+        results.push(string);
+        return results;
+    }
+
+    for (let index = 0; index < string.length; index++) {
+        const firstChar = string[index];
+        let charLeft = string.slice(0, index) + string.slice(index+1);
+        let innerPermutations = getPermutations(charLeft);
+
+        for (let j = 0; j < innerPermutations.length; j++) {
+            results.push(firstChar + innerPermutations[j]);
+        }
+    }
+
+    return results;
+}
+
+console.log('All possible combinations of string "abc"', getPermutations('abc'));
 
 
 
